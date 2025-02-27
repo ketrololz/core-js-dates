f5439d4105e3efadf9c118efdef31933c43e8f10
ketrololz <bqnexxx@gmail.com>
2025-02-28 01:29:39 +0300

    complete tasks 13-14

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -297,8 +297,29 @@ function getQuarter(date) {
  * { start: '01-01-2024', end: '15-01-2024' }, 1, 3 => ['01-01-2024', '05-01-2024', '09-01-2024', '13-01-2024']
  * { start: '01-01-2024', end: '10-01-2024' }, 1, 1 => ['01-01-2024', '03-01-2024', '05-01-2024', '07-01-2024', '09-01-2024']
  */
-function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
-  throw new Error('Not implemented');
+function getWorkSchedule(period, countWorkDays, countOffDays) {
+  const startArr = period.start.split('-');
+  const endArr = period.end.split('-');
+  const startDate = new Date(
+    Date.UTC(startArr[2], startArr[1] - 1, startArr[0])
+  );
+  const endDate = new Date(Date.UTC(endArr[2], endArr[1] - 1, endArr[0]));
+  const days = [];
+  const msInOneDay = 3600 * 24 * 1000;
+  let counter = countWorkDays;
+
+  while (startDate.getTime() <= endDate.getTime()) {
+    days.push(startDate.toLocaleString().split(',')[0].split('.').join('-'));
+    counter -= 1;
+    if (counter === 0) {
+      startDate.setTime(startDate.getTime() + (countOffDays + 1) * msInOneDay);
+      counter = countWorkDays;
+    } else {
+      startDate.setTime(startDate.getTime() + msInOneDay);
+    }
+  }
+
+  return days;
 }
 
 /**
@@ -313,8 +334,11 @@ function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
  * Date(2022, 2, 1) => false
  * Date(2020, 2, 1) => true
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  return (
+    (date.getUTCFullYear() % 4 === 0 && date.getUTCFullYear() % 100 !== 0) ||
+    date.getUTCFullYear() % 400 === 0
+  );
 }
 
 module.exports = {
